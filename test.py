83f9b913feda242ef58557443ec51e85e945c00b
NaCl <1375186232@qq.com>
2024-08-31 18:24:06 +0800

    build(ChartTTS): 🛠️ GPU构建

---
+++ test.py
@@ -0,0 +1,6 @@
+import torch
+
+if __name__ == '__main__':
+    print("123")
+    print("print(torch.__version__)", torch.__version__)
+    # print("torch.cuda.is_available()", torch.cuda.is_available())
